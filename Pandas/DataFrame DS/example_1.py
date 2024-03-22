23401861b7d569e9cb34b275430e86e1b3c41258
Celyne <dianakydd2@gmail.com>
2024-03-22 15:23:21 +0300

    DataFrame eample

---
+++ Pandas/DataFrame DS/example_1.py
@@ -0,0 +1,9 @@
+import pandas as pd
+df = pd.DataFrame({
+    'Name':["Celyne", "Naomi", "Victor"],
+    'Age' :[30,50,40],
+    'Subject' :['Python', 'Ruby', 'JS'],
+    "DOB" : [200,2023,2021]
+})
+
+print(df)
